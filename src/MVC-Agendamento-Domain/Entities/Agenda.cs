8aee504e693ff47d006875aee23ec1ea44b1ec2e
Patrick Weber <patrickrenanweber@gmail.com>
2023-02-09 22:07:16 -0300

    Criacao entity e DTO

---
+++ src/MVC-Agendamento-Domain/Entities/Agenda.cs
@@ -0,0 +1,22 @@
+﻿using MVC_Agendamento_Domain.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Agendamento_Domain.Entities
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public int PacienteId { get; set; }
+        public int MedicoId { get; set; }
+        public bool ConsultaConfirmada { get; set; }
+        public EnumStatus Status { get; set; }
+        public string Observacoes { get; set; }
+
+        public virtual string? Paciente { get; set; }  //Será alterado para a entidade correta
+        public virtual string? Medico { get; set; }
+    }
+}
