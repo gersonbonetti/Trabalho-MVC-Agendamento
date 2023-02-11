1782b311ebbef593f281670383cc49b94bdbbcbd
Jailsonr12 <104799776+Jailsonr12@users.noreply.github.com>
2023-02-10 21:04:07 -0300

    Atendimento
    Atendimento

---
+++ src/MVC-Agendamento-Domain/DTO/ServiceDTO.cs
@@ -0,0 +1,47 @@
+﻿using MVC_Agendamento_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Agendamento_Domain.DTO {
+    internal class ServiceDTO {
+
+        public int id { get; set; }
+        public int idSchedule { get; set; }
+        public int idPatient { get; set; }
+        public int idDoctor { get; set; }
+        public virtual string? patient { get; set; }
+        public virtual string? doctor { get; set; }
+        public int numberService { get; set; }
+        public DateTime date { get; set; }
+
+
+        public Service mapToEntity() {
+            return new Service {
+                Id = id,
+                IdSchedule = idSchedule,
+                IdPatient = idPatient,
+                IdDoctor = idDoctor,
+                Patient = patient,
+                Doctor = doctor,
+                NumberService = numberService,
+                Date = date,
+            };
+        }
+        public ServiceDTO mapToDTO(Service service) {
+            return new ServiceDTO {
+                idSchedule = service.IdSchedule,
+                idPatient = service.IdPatient,
+                idDoctor = service.IdDoctor,
+                patient = service.Patient,
+                doctor = service.Doctor,
+                numberService = service.NumberService,
+                date = service.Date,
+            };
+
+        }
+
+    }
+}
---
+++ src/MVC-Agendamento-Domain/Entities/Service.cs
@@ -0,0 +1,24 @@
+﻿using MVC_Agendamento_Domain.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Agendamento_Domain.Entities {
+    internal class Atendimento {
+
+        public int Id { get; set; }
+
+        public int IdSchedule { get; set; }
+        public int IdPatient { get; set; }
+        public int IdDoctor { get; set; }
+        public DateTime Date { get; set; }
+
+
+
+
+
+
+    }
+}
