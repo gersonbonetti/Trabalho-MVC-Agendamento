fc1eee6db71a7d47afa24ceb788b4c4362cb180e
Jailsonr12 <104799776+Jailsonr12@users.noreply.github.com>
2023-02-10 21:11:01 -0300

    pipocca

--- src/MVC-Agendamento-Domain/Entities/Service.cs
+++ src/MVC-Agendamento-Domain/Entities/Service.cs
@@ -6,19 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MVC_Agendamento_Domain.Entities {
-    internal class Atendimento {
+    internal class Service {
 
         public int Id { get; set; }
-
         public int IdSchedule { get; set; }
         public int IdPatient { get; set; }
         public int IdDoctor { get; set; }
+        public virtual string? Patient { get; set; }
+        public virtual string? Doctor { get; set; }
+        public int NumberService { get; set; }
         public DateTime Date { get; set; }
-
-
-
-
-
 
     }
 }
